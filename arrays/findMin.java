bb1e637ea2e0da23df5508f1d697db58f72fd331
SohanSonu303 <87355196+SohanSonu303@users.noreply.github.com>
2025-02-09 00:00:40 +0530

    testchanges

---
+++ arrays/findMin.java
@@ -0,0 +1,19 @@
+package arrays;
+
+public class findMin {
+    public static void main(String[] args){
+        System.out.println(findMin(new int[]{3,4,5,1,2}));
+        System.out.println(findMin(new int[]{4,5,6,7,0,1,2}));
+        System.out.println(findMin(new int[]{11,13,15,17}));
+    }
+    public static int findMin(int[] nums) {
+        int ans = Integer.MAX_VALUE;
+        int prev = Integer.MAX_VALUE;
+
+        for(int i = 0 ; i< nums.length ;i ++){
+            if(ans > nums[i]){
+                ans = nums[i];
+            }
+        }
+    }
+}
